80c8acc6c7e4129268e07055b55942a6326d852c
Duyen <duyenk200@gmail.com>
2026-01-06 08:56:58 +0700

    fix

--- openCV/build/intermediates/cxx/Debug/2xk22prw/logs/arm64-v8a/build_command_opencv_jni_shared.bat
+++ openCV/build/intermediates/cxx/Debug/2xk22prw/logs/arm64-v8a/build_command_opencv_jni_shared.bat
@@ -1,5 +1,5 @@
 @echo off
 "C:\\Users\\LVL_TX2\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
   -C ^
-  "D:\\Count\\APP\\USBCAM_Android_app\\openCV\\.cxx\\Debug\\2xk22prw\\arm64-v8a" ^
+  "C:\\Users\\LVL_TX2\\AndroidStudioProjects\\usbcam\\openCV\\.cxx\\Debug\\2xk22prw\\arm64-v8a" ^
   opencv_jni_shared
