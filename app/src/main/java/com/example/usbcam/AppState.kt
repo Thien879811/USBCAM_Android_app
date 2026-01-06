0ef1bf392ea54601f5ee4ef3c571639e6191dd0d
Thien <thienlhg0318@gmail.com>
2026-01-06 10:48:09 +0700

    git commit -m "Remove openCV from tracking"

--- app/src/main/java/com/example/usbcam/AppState.kt
+++
@@ -1,9 +0,0 @@
-package com.example.usbcam
-
-enum class AppState {
-    IDLE,
-    PROCESSING,
-    VALIDATING,
-    SUCCESS,
-    ERROR_LOCKED
-}
--- app/src/main/java/com/example/usbcam/viewmodel/TargetData.kt
+++
@@ -1,7 +0,0 @@
-package com.example.usbcam.viewmodel
-
-data class TargetData(
-    val qtyByLean: String,
-    val scgs: String,
-    val quantityTarget: Int
-)
