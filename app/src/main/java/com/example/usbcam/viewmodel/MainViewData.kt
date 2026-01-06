a3f817654cc06356acd86fd66fb3f54d7a1de6ea
Thien <thienlhg0318@gmail.com>
2026-01-06 10:20:21 +0700

    chore: lib

--- app/src/main/java/com/example/usbcam/viewmodel/MainViewData.kt
+++
@@ -1,7 +0,0 @@
-package com.example.usbcam.viewmodel
-
-data class MainViewData(
-    val frameTime: String,
-    val target: Int,
-    val quantity: Int
-)
--- app/src/main/java/com/example/usbcam/viewmodel/TimeSlotItem.kt
+++
@@ -1,8 +0,0 @@
-package com.example.usbcam.viewmodel
-
-data class TimeSlotItem(
-    val index: Int,
-    val frameTime: String,
-    val target: Int,
-    val quantity: Int
-)
